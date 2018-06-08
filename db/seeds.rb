0e10aaef2a8f53aac848221d6ecf2fae3efa9b8f
Adilson Angelo <aasj2@cin.ufpe.br>
2018-06-08 19:44:06 -0300

    DB and test factory setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def sandwich(sentences = 3, repeat = 1)
+    text_block = []
+    text_block << Faker::Markdown.headers
+    repeat.times do
+        text_block << Faker::Lorem.paragraph(sentences)
+        text_block << Faker::Markdown.random
+    end
+    text_block.join("\n")
+end
+
+5.times do
+    Document.create({
+        body: sandwich(5, 3)
+    })
+end
